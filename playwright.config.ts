9508c122cf5eeddcddbdd1826ce0f77d62395c26
Miguel-Barca <fugu4u@gmail.com>
2025-08-01 12:22:26 +0100

    feat: provide html report on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [['list']]
+    ? [['list'], ['html']]
     : [['list', { printSteps: true }], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
